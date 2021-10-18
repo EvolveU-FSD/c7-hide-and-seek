2ffa371012b2930180527d2865b9b70d38ca0cde
Tony Enerson <tony@enerson.ca>
2021-10-18 10:28:08 -0600

    Working game model... no express yet

---
+++ hideAndSeek.js
@@ -0,0 +1,27 @@
+let rooms = ['living room', 'dining room']
+let hiderLocation
+let seekerLocation
+
+function listRooms() {
+    return rooms
+}
+
+function startGame() {
+    hiderLocation = rooms[Math.floor(Math.random()*rooms.length)]
+}
+
+function move(room) {
+    console.log('Seeker moved to ', room)
+    seekerLocation = room
+}
+
+function search() {
+    return (seekerLocation === hiderLocation)
+}
+
+module.exports = {
+    listRooms,
+    startGame,
+    move,
+    search
+}
---
+++ testGame.js
@@ -0,0 +1,18 @@
+let hideAndSeek = require('./hideAndSeek')
+
+// play a game
+console.log('Starting a game')
+hideAndSeek.startGame()
+
+let rooms = hideAndSeek.listRooms()
+console.log('We can search ', rooms)
+hideAndSeek.move(rooms[0])
+
+let found = hideAndSeek.search()
+if (found) {
+    console.log('We found the hider!')
+}
+else {
+    console.log('We did not find the hider!')
+    console.log('We should have searched the', rooms[1])
+}
